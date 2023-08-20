9d144aa9a5d396d3a2d491d049f32513a5282d3a
Aiden Chang <aidenchang@pcpartner.com>
2023-08-20 11:10:38 +0800

    Init project

---
+++ app/src/main/java/com/adncoding/learn_compose_land_of_coding/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.adncoding.learn_compose_land_of_coding
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.adncoding.learn_compose_land_of_coding.ui.theme.Learn_compose_landofcodingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Learn_compose_landofcodingTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Learn_compose_landofcodingTheme {
+        Greeting("Android")
+    }
+}
